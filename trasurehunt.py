b4fe64fc5f1fb05c8ef10b0dc3c8f43c3b7a16ec
byebri <156145093+byebri@users.noreply.github.com>
2024-01-29 19:41:07 -0800

    Create trasurehunt.py

---
+++ trasurehunt.py
@@ -0,0 +1,20 @@
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+letter = position[0]
+number = int(position[1]) - 1
+if letter == "A":
+    letter_num = 0
+elif letter == "B":
+    letter_num = 1
+else:
+    letter_num = 2
+map[number][letter_num] = "X"
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
